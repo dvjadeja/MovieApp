92e5df219c58fa23a6618f7ff7cf648cf8a82280
Dilipsinh Jadeja <70471636+dvjadeja@users.noreply.github.com>
2021-05-23 09:05:39 +0530

    Database Connection and POST request

--- movieAppBackEnd/routes/routes.js
+++ movieAppBackEnd/routes/routes.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const signUpTemplateCopy = require('../models/fillMongoose')
 
-router.post('/fill', (request, response) => )
+router.post('/signup', (request, response) => {
+    const signedUpUser = new signUpTemplateCopy({
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
+    })
+    signedUpUser.save()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        })
+});
+
+module.exports = router;
